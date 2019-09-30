265267390f26c214338e54a4feb791e94cca66c9
Mindexperiment <andrewloyd@libero.it>
2019-09-30 19:06:31 +0200

    close #3 - add article body on public view

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -1,3 +1,11 @@
 <article>
-    <h2>{{ $article->title }}</h2>
+    <header>
+        <h2>{{ $article->title }}</h2>
+
+        <p><small>{{ $article->author->name }} - {{ $article->updated_at->diffForHumans() }} ({{ $article->created_at->toFormattedDateString() }})</small></p>
+    </header>
+
+    <div>
+        {{ Illuminate\Mail\Markdown::parse($article->body) }}
+    </div>
 </article>
